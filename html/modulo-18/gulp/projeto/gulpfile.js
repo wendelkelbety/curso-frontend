4707b178620647516a5f6e5b27677a0ec51651e1
Wendel <wendelkleberty@gmail.com>
2023-05-15 16:59:15 -0300

    modulo-18 completo

--- html/modulo-18/gulp/projeto/gulpfile.js
+++ html/modulo-18/gulp/projeto/gulpfile.js
@@ -3,9 +3,16 @@ const concat = require("gulp-concat")
 const cssmin = require("gulp-cssmin")
 const rename = require("gulp-rename")
 const uglify = require("gulp-uglify")
+const image = require("gulp-imagemin")
 
 function tarefaCSS(){
-    return gulp.src('./vendor/**/*.css')
+    //return gulp.src('./vendor/**/*.css')
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './vendor/owl/owl.carousel.min.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/jquery-ui/jquery-ui.min.css',
+        './assets/css/style.css'])
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({suffix:'.min'}))
@@ -13,12 +20,36 @@ function tarefaCSS(){
 }
 
 function tarefaJS(){
-    return gulp.src('./vendor/**/*.js')
+    //return gulp.src('./vendor/**/*.js')
+    return gulp.src([
+        './node_modules/jquery/jquery-3.7.0.min.js',
+        './vendor/bootstrap-5.3.0-alpha3-dist/js/bootstrap.js',
+        './vendor/owl/owl.carousel.min.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './assets/js/custom.js'])
     .pipe(concat('libs.js'))
     .pipe(uglify())
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('./dist/js'))
 }
 
+function tarefasImagem(){
+    return gulp.src('./assets/images/*')
+    .pipe(image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: true
+    }))
+    .pipe(gulp.dest('./dist/images'))
+}
+
 exports.styles = tarefaCSS
 exports.scripts = tarefaJS
+exports.images = tarefasImagem
