18f66841f357205778c32f6eca1726e95cf36d53
wendel kelbety de melo iwashita <wendelkleberty@gmail.com>
2023-05-24 20:19:59 -0300

    exercicio modulo-25

--- html/modulo-25/javascript-main/src/modulesExport.js
+++ html/modulo-25/javascript-main/src/modulesExport.js
@@ -8,9 +8,7 @@ export { cores, getName, Animais}
 
 // strict mode > como ficam todos os exports 
 
-export default getCars() {
-
-}
+//export default getCars() {}
 
 
-// export default getCars
+// export default getCars{}
