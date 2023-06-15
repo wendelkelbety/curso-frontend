36fd351ec7622b8ba7d986827fa242970113993e
Wendel <wendelkleberty@gmail.com>
2023-06-15 11:14:03 -0300

    modulo-33 fim

--- html/modulo-33/app/src/App.js
+++ html/modulo-33/app/src/App.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 import Dashboard from './components/dashboard'
--- html/modulo-33/app/src/App.test.js
+++ html/modulo-33/app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('teste titulo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const testCase = document.querySelector('.App h1');
+  expect(testCase).toHaveTextContent(/meu painel de controle/i);
 });
