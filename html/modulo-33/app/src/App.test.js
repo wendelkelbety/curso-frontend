b3c3882b05b9c5e898caa8e0e8c80b5e97e22c75
Wendel <wendelkleberty@gmail.com>
2023-06-15 15:50:30 -0300

    teste

--- html/modulo-33/app/src/App.test.js
+++ html/modulo-33/app/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('teste titulo', () => {
-  render(<App />);
+  render(<App/>);
   const testCase = document.querySelector('.App h1');
   expect(testCase).toHaveTextContent(/meu painel de controle/i);
 });
